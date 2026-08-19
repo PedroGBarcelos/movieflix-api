e5355c64cf95dd5ccce0fcfdb7af1142663ce43f
Pedro Guimaraes <pedrosbarcelos98@gmail.com>
2026-08-19 02:22:47 -0300

    adicionada a atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -21,8 +21,8 @@ app.post('/movies', async (req, res) => {
 
     try {
         const movieWithSameTitle = await prisma.movie.findFirst({
-            where: { title: { equals: title, mode: "insensitive"} },
-        });
+            where: { title: { equals: title, mode: 'insensitive' } },
+        })
 
         if (movieWithSameTitle) {
             return res.status(409).send({ message: 'Filme com o mesmo título já existe.' })
@@ -42,8 +42,36 @@ app.post('/movies', async (req, res) => {
     }
 
     res.status(201).json({ message: 'Filme criado com sucesso!' })
-})
+});
 
+app.put('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id)
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        })
+
+        if (!movie) {
+            return res.status(404).send({ message: 'Filme não encontrado.' })
+        }
+
+        const data = { ...req.body }
+        data.release_date = data.release_date
+            ? new Date(data.release_date)
+            : undefined
+
+        await prisma.movie.update({
+            where: { id },
+            data: data,
+        })
+    } catch {
+        return res.status(500).send({ message: 'Erro ao atualizar o registro do filme.' })
+    }
+
+    res.status(200).send()
+}),
+    
 app.listen(port, () => {
-    console.log(`Servidor em execução em http://localhost:${port}`)
+        console.log(`Servidor em execução em http://localhost:${port}`)
 })
