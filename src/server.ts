1862a93420bd593ca7924b890baf1d28c2f71072
Pedro Guimaraes <pedrosbarcelos98@gmail.com>
2026-08-19 01:35:59 -0300

    adicionada mensagem de erro na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,41 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
 
-const port = 3000;
-const app = express();
-const prisma = new PrismaClient();
+const port = 3000
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(express.json())
 
 app.get('/movies', async (_, res) => {
- const movies = await prisma.movie.findMany({
-   orderBy: { title: 'asc' },
-   include: { genres: true, languages: true },
- });
- res.json(movies);
-});  
+    const movies = await prisma.movie.findMany({
+        orderBy: { title: 'asc' },
+        include: { genres: true, languages: true },
+    })
+    res.json(movies)
+})
+
+app.post('/movies', async (req, res) => {
+
+   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+   try {
+    await prisma.movie.create({
+        data: {
+            title,
+            genre_id,
+            language_id,
+            oscar_count,
+            release_date: new Date(release_date)
+         },
+    })
+   }catch{
+      return res.status(500).send({ message: 'Erro ao criar o filme.' })
+   }
+
+    res.status(201).json({ message: 'Filme criado com sucesso!' })
+})
 
 app.listen(port, () => {
-   console.log(`Servidor em execução em http://localhost:${port}`);
-});
-
+    console.log(`Servidor em execução em http://localhost:${port}`)
+})
